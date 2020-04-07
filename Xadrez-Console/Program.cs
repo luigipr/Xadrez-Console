24659d2d80ecff72845c7df0677e4f7cdda5c692
luigipr <luigiprofeti@gmail.com>
2020-04-07 15:50:27 -0300

    Tratando erros com excessao personalizada

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -6,17 +6,22 @@ namespace Xadrez_Console {
     class Program {
         static void Main(string[] args) {
 
+            try {
+                Board tab = new Board(8, 8);
 
-            Board tab = new Board(8, 8);
+                tab.dropPiece(new Tower(tab, Color.Black), new Position(0, 0));
+                tab.dropPiece(new Tower(tab, Color.Black), new Position(1, 9));
+                tab.dropPiece(new King(tab, Color.Black), new Position(2, 4));
+                tab.dropPiece(new King(tab, Color.Black), new Position(0, 2));
 
-            tab.dropPiece(new Tower(tab, Color.Black) , new Position(0, 0));
-            tab.dropPiece(new Tower(tab, Color.Black) , new Position(1, 3));
-            tab.dropPiece(new King(tab, Color.Black) , new Position(2, 4));
-            
+
 
 
             Screen.printBoard(tab);
-
+            }
+            catch (BoardException e) {
+                Console.WriteLine(e.Message);
+            }
 
 
 
--- Xadrez-Console/board/Board.cs
+++ Xadrez-Console/board/Board.cs
@@ -18,10 +18,34 @@ namespace board {
             return Pieces[line, column];
         }
 
+        public Piece piece(Position pos) {
+            return Pieces[pos.lines, pos.columns];
+                }
+
+        public bool ExistingPiece(Position pos) {
+            ValidatePosition(pos);
+            return piece(pos) != null;
+        }
+
         public void dropPiece(Piece p, Position pos) {
+            if (ExistingPiece(pos)) {
+                throw new BoardException("There is already a piece in this position!");
+            }
             Pieces[pos.lines, pos.columns] = p;
             p.Position = pos;
         }
 
+        public bool ValidPosition(Position pos) {
+            if (pos.lines < 0 || pos.lines >= Lines || pos.columns < 0 || pos.columns >= Columns) {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidatePosition(Position pos) {
+            if (!ValidPosition(pos)) {
+                throw new BoardException("Invalid Position!");
+            }
+        }
     }
 }
